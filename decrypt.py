3760e9d631011c3af198fc2d81a7e177f3b9ffdc
Razvan Dumitriu <razvan@dumitriu.co.uk>
2021-11-23 15:10:29 +0200

    Crypt/Decrypt done

--- decrypt.py
+++ decrypt.py
@@ -1,22 +1,31 @@
 from itertools import cycle
+import argparse
+
+parser = argparse.ArgumentParser(description="Criptare XOR.")
+parser.add_argument('keyPath', metavar='parola', type=str, help='Fisierul care contine parola')
+parser.add_argument('inputPath', metavar='intrare', type=str, help='Fisierul care contine inputul')
+parser.add_argument('outputPath', metavar='iesire', type=str, help='Fisierul care contine outputul')
+
+args = parser.parse_args()
+# print(args)
 
 keyFile = inputFile = outputFile = ''
 try:
-    keyFile = open("parola", "r")
+    keyFile = open(args.keyPath, "r")
 except FileNotFoundError:
     print("Nu exista fisierul parola!")
     exit(1)
 
 try:
-    inputFile = open("output", "r")
+    inputFile = open(args.inputPath, "r")
 except FileNotFoundError:
     print("Nu exista fisierul input.txt!")
     exit(1)
 
-outputFile = open("input.txt", "w")
+outputFile = open(args.outputPath, "w")
 
 passwd = keyFile.readline()  # Citesc parola
 aux = inputFile.read()
 s = [aux[i:i + 8] for i in range(0, len(aux), 8)]
 for a, b in zip(s, cycle(passwd)):
-    print(chr((int(a, 2) ^ ord(b))), end='')
+    outputFile.write(chr((int(a, 2) ^ ord(b))))
--- encrypt.py
+++ encrypt.py
@@ -1,19 +1,28 @@
 from itertools import cycle
+import argparse
+
+parser = argparse.ArgumentParser(description="Criptare XOR.")
+parser.add_argument('keyPath', metavar='parola', type=str, help='Fisierul care contine parola')
+parser.add_argument('inputPath', metavar='intrare', type=str, help='Fisierul care contine inputul')
+parser.add_argument('outputPath', metavar='iesire', type=str, help='Fisierul care contine outputul')
+
+args = parser.parse_args()
+# print(args)
 
 keyFile = inputFile = outputFile = ''
 try:
-    keyFile = open("parola", "r")
+    keyFile = open(args.keyPath, "r")
 except FileNotFoundError:
     print("Nu exista fisierul parola!")
     exit(1)
 
 try:
-    inputFile = open("input.txt", "r")
+    inputFile = open(args.inputPath, "r")
 except FileNotFoundError:
     print("Nu exista fisierul input.txt!")
     exit(1)
 
-outputFile = open("output", "w")
+outputFile = open(args.outputPath, "w")
 
 passwd = keyFile.readline()  # Citesc parola
 s = inputFile.read()  # Incarc inputul
