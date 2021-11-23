80adf07c7e261710ad2b10b872cc301f64e54add
Razvan Dumitriu <razvan@dumitriu.co.uk>
2021-11-23 13:45:43 +0200

    Added project

---
+++ encrypt.py
@@ -0,0 +1,21 @@
+from itertools import cycle
+
+keyFile = inputFile = outputFile = ''
+try:
+    keyFile = open("parola", "r")
+except FileNotFoundError:
+    print("Nu exista fisierul parola!")
+    exit(1)
+
+try:
+    inputFile = open("input.txt", "r")
+except FileNotFoundError:
+    print("Nu exista fisierul input.txt!")
+    exit(1)
+
+outputFile = open("output", "w")
+
+passwd = keyFile.readline()  # Citesc parola
+s = inputFile.read()  # Incarc inputul
+for a, b in zip(s, cycle(passwd)):
+    outputFile.write(format((ord(a) ^ ord(b)), '08b'))  # Aplic XOR si afisez pe 8 biti
