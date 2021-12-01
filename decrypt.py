d4fc8299c9d72d501baabf3e05a9468d039d63a0
Razvan Dumitriu <razvan@dumitriu.co.uk>
2021-12-02 00:13:35 +0200

    Project done

--- decrypt.py
+++ decrypt.py
@@ -2,30 +2,24 @@ from itertools import cycle
 import argparse
 
 parser = argparse.ArgumentParser(description="Criptare XOR.")
-parser.add_argument('keyPath', metavar='parola', type=str, help='Fisierul care contine parola')
+parser.add_argument('key', metavar='parola', type=str, help='Parola de decriptare')
 parser.add_argument('inputPath', metavar='intrare', type=str, help='Fisierul care contine inputul')
 parser.add_argument('outputPath', metavar='iesire', type=str, help='Fisierul care contine outputul')
 
 args = parser.parse_args()
 # print(args)
 
-keyFile = inputFile = outputFile = ''
-try:
-    keyFile = open(args.keyPath, "r")
-except FileNotFoundError:
-    print(f"Nu exista fisierul {args.keyPath}!")
-    exit(1)
+inputFile = outputFile = ''
 
 try:
-    inputFile = open(args.inputPath, "r")
+    inputFile = open(args.inputPath, "rb")
 except FileNotFoundError:
     print(f"Nu exista fisierul {args.inputPath}!")
     exit(1)
 
 outputFile = open(args.outputPath, "w")
 
-passwd = keyFile.readline()  # Citesc parola
-aux = inputFile.read()
-s = [aux[i:i + 8] for i in range(0, len(aux), 8)]
-for a, b in zip(s, cycle(passwd)):
-    outputFile.write(chr((int(a, 2) ^ ord(b))))
+passwd = str(args.key)  # Citesc parola
+s = str(inputFile.read().decode("utf8"))  # Incarc inputul
+decrypted = ''.join(chr(ord(a) ^ ord(b)) for a, b in zip(s, cycle(passwd)))
+outputFile.write(decrypted)
--- encrypt.py
+++ encrypt.py
@@ -2,19 +2,14 @@ from itertools import cycle
 import argparse
 
 parser = argparse.ArgumentParser(description="Criptare XOR.")
-parser.add_argument('keyPath', metavar='parola', type=str, help='Fisierul care contine parola')
+parser.add_argument('key', metavar='parola', type=str, help='Parola de criptare')
 parser.add_argument('inputPath', metavar='intrare', type=str, help='Fisierul care contine inputul')
 parser.add_argument('outputPath', metavar='iesire', type=str, help='Fisierul care contine outputul')
 
 args = parser.parse_args()
 # print(args)
 
-keyFile = inputFile = outputFile = ''
-try:
-    keyFile = open(args.keyPath, "r")
-except FileNotFoundError:
-    print(f"Nu exista fisierul {args.keyPath}!")
-    exit(1)
+inputFile = outputFile = ''
 
 try:
     inputFile = open(args.inputPath, "r")
@@ -22,9 +17,9 @@ except FileNotFoundError:
     print(f"Nu exista fisierul {args.inputPath}!")
     exit(1)
 
-outputFile = open(args.outputPath, "w")
+outputFile = open(args.outputPath, "wb")
 
-passwd = keyFile.readline()  # Citesc parola
-s = inputFile.read()  # Incarc inputul
-for a, b in zip(s, cycle(passwd)):
-    outputFile.write(format((ord(a) ^ ord(b)), '08b'))  # Aplic XOR si afisez pe 8 biti
+passwd = str(args.key)  # Citesc parola
+s = str(inputFile.read())  # Incarc inputul
+crypted = ''.join(chr(ord(a) ^ ord(b)) for a, b in zip(s, cycle(passwd)))
+outputFile.write(crypted.encode("utf8", errors='replace'))
